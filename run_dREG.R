8670b39a5dc35b0a8fad70e74b983c80b5c3aec0
dREG Community User <dreg2016@comet-ln2.sdsc.edu>
2017-09-07 16:06:24 -0700

    gateway using new dREG model on Sep 2017

--- run_dREG.R
+++ run_dREG.R
@@ -39,61 +39,27 @@ cat("SRC PATH:", src_path, "\n");
 
 ## Now scan all positions in the genome ...
 cat("1) -------- Checking the informative positions\n");
-inf_positions <- get_informative_positions(ps_plus_path, ps_minus_path, depth= 0, step=50, use_ANDOR=TRUE, use_OR=FALSE) ## Get informative positions.
-cat("Genome Loci=", NROW(inf_positions), "\n");
+load("./newmodel/asvm.gdm.6.6M.20170828.rdata");
+r <- peak_calling( asvm, gdm, ps_plus_path, ps_minus_path, ncores=ncores, use_rgtsvm=use_rgtsvm)
 
+outfile <- "out"
+out.file1 <- paste(outfile, "dREG.infp.bed", sep=".")
+out.file2 <- paste(outfile, "dREG.peak.full.bed", sep=".")
+out.file3 <- paste(outfile, "dREG.peak.score.bed", sep=".")
 
-cat("2) -------- Prediction in dREG model\n");
-t <- system.time( pred_val<- eval_reg_svm(gdm, asvm, inf_positions, ps_plus_path, ps_minus_path, batch_size= 50000, ncores=ncores, use_rgtsvm=use_rgtsvm, use_snowfall=TRUE) )
-cat("Running time [User]:", t[1], "[System]:", t[2], "[Elapsed]:", t[3], "\n");
-
-
-cat("3) -------- write prediction data\n");
-pred_data <- data.frame( inf_positions, pred_val )
-file.dreg.pred.gz <- paste( outfile, ".dREG.pred.gz", sep="" );
-gz1 <- gzfile( file.dreg.pred.gz, "w");
-write.table(pred_data, gz1, row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t");
-close(gz1)
-
-cat("4) -------- Merge peaks\n");
-system( paste( "bash ", src_path, "/writeBed.bsh", " 0.15 ", file.dreg.pred.gz, sep="") );
-
-file.dreg.peak.gz <- paste(outfile, ".dREG.peak.gz", sep="");
-file.rename(paste(file.dreg.pred.gz, ".bed.gz", sep=""), file.dreg.peak.gz)
-unlink(file.dreg.pred.gz);
-
-cat("5) -------- Refining in dREG-HD model\n");
-require(dREG.HD);
-## load drEG_HD model
-load(args[5]);
-t <- system.time( dREG_HD( bed_path = file.dreg.peak.gz, bigwig_plus = ps_plus_path, bigwig_minus = ps_minus_path, model=model, ncores = ncores, use_rgtsvm= use_rgtsvm ) )
-cat("Running time [User]:", t[1], "[System]:", t[2], "[Elapsed]:", t[3], "\n");
-
-file.rename( paste(file.dreg.peak.gz, "_imputedDnase.bw", sep=""), paste(outfile, "dREG.HD.imputedDnase.bw", sep=".") )
-file.rename( paste(file.dreg.peak.gz, "_dREG_HD_relaxed.bed", sep=""), paste(outfile, "dREG.HD.relaxed.bed", sep=".") )
-file.rename( paste(file.dreg.peak.gz, "_dREG_HD_stringent.bed", sep=""), paste(outfile, "dREG.HD.stringent.bed", sep=".") )
-
-make_index_gz<-function(df_bed, outfile, file_id)
+make_index_gz<-function( df_bed, outfile)
 {
-	write.table( df_bed, file=paste( outfile, ".", file_id, sep=""), row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t");
-	system(paste( "bgzip -f ", outfile, ".", file_id, sep="") );
-	system(paste( "tabix -f -p bed ", outfile, ".", file_id, ".gz", sep="") );
+	file.tmp <- tempfile(fileext=".bed");
+	write.table( df_bed, file=file.tmp, row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t");
+	system(paste( "sort-bed ", file.tmp, " | bgzip -f > " outfile, ".gz", sep="") );
+	system(paste( "tabix -f -p bed ", outfile, ".gz", sep="") );
+	unlink(file.tmp)
 }
-make_index_gz(pred_data, outfile, "dREG.pred");
 
-tb <- read.table(file.dreg.peak.gz, header = FALSE);
-tb <- tb[,-4];
+make_index_gz( r$infp_bed, out.file1 );
+make_index_gz( r$peak_bed, out.file2 );
+make_index_gz( r$peak_bed[,c(1:4)], out.file3 );
 
-unlink( file.dreg.peak.gz );
-make_index_gz( tb, outfile, "dREG.peak");
-
-tb <- read.table(paste(outfile, "dREG.HD.relaxed.bed", sep="."));
-tb <- data.frame(tb, 1);
-make_index_gz( tb, outfile, "dREG.HD.relaxed.bed");
-
-tb <- read.table(paste(outfile, "dREG.HD.stringent.bed", sep="."));
-tb <- data.frame(tb, 1);
-make_index_gz( tb, outfile, "dREG.HD.stringent.bed");
 
 system( paste("tar -cvzf ", outfile, ".dREG.tar.gz", " ", outfile, ".dREG.*", sep="") );
 cat("Result:", paste(outfile, ".dREG.tar.gz", sep=""), "\n");
