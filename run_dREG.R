167df14c35ce7a75c4ad06f83482c3168c5d495d
dREG Community User <dreg2016@comet-ln3.sdsc.edu>
2018-09-24 10:25:52 -0700

    modified based on new dREG

--- run_dREG.R
+++ run_dREG.R
@@ -111,7 +111,7 @@ out.chrom.info <- paste(outfile, "chrom.info", sep=".")
 
 
 make.chrom.info(ps_plus_path, ps_minus_path, out.chrom.info);
-make_index_gz( r$raw_peak[ r$raw_peak$prob != -1, -2 ], out.raw.peak.bed);
+make_index_gz( r$raw_peak[ r$raw_peak$prob != -1, ], out.raw.peak.bed);
 make_index_gz( r$infp_bed, out.infp.bed );
 make_bw( r$infp_bed[,1:4], out.infp.bw );
 make_index_gz( r$peak_bed, out.full.bed );
